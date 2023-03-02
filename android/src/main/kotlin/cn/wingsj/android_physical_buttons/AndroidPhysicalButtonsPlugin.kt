fcae2c9e5fc336d7252ac1b662e57466eacf6c1c
RehamZaky <41897989+RehamZaky@users.noreply.github.com>
2023-03-02 17:15:32 +0200

    Update AndroidPhysicalButtonsPlugin.kt

--- android/src/main/kotlin/cn/wingsj/android_physical_buttons/AndroidPhysicalButtonsPlugin.kt
+++ android/src/main/kotlin/cn/wingsj/android_physical_buttons/AndroidPhysicalButtonsPlugin.kt
@@ -4,11 +4,14 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.view.KeyEvent
 import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.EventChannel
 
-class AndroidPhysicalButtonsPlugin : FlutterPlugin, EventChannel.StreamHandler, BroadcastReceiver() {
+class AndroidPhysicalButtonsPlugin : FlutterPlugin, ActivityAware, EventChannel.StreamHandler, BroadcastReceiver() {
     private var actionVolume = "android.media.VOLUME_CHANGED_ACTION"
     private lateinit var channel: EventChannel
     private var sink: EventChannel.EventSink? = null
@@ -40,5 +43,31 @@ class AndroidPhysicalButtonsPlugin : FlutterPlugin, EventChannel.StreamHandler, 
         } else if (intent.action.equals(actionVolume)) {
             sink?.success("volume");
         }
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        binding.activity.onKeyUp(
+            KeyEvent.KEYCODE_VOLUME_UP,
+            KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_VOLUME_UP)
+        ).let {
+            sink?.success("volume")
+        }
+
+        binding.activity.onKeyDown(
+            KeyEvent.KEYCODE_VOLUME_DOWN,
+            KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_VOLUME_DOWN)
+        ).let {
+            sink?.success("volume")
+        }
+    }
+
+
+    override fun onDetachedFromActivityForConfigChanges() {
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    }
+
+    override fun onDetachedFromActivity() {
     }
 }
